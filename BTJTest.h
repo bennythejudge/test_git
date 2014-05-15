fed124feec0e5682983b0bb43c108e9f683c2eb5
logube <m253ou@gmail.com>
2014-05-15 20:29:48 +0100

    added a new class

---
+++ BTJTest.h
@@ -0,0 +1,13 @@
+//
+//  BTJTest.h
+//  Git Test
+//
+//  Created by neo on 15/05/2014.
+//  Copyright (c) 2014 BennyTheJudge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BTJTest : NSObject
+
+@end
---
+++ BTJTest.m
@@ -0,0 +1,13 @@
+//
+//  BTJTest.m
+//  Git Test
+//
+//  Created by neo on 15/05/2014.
+//  Copyright (c) 2014 BennyTheJudge. All rights reserved.
+//
+
+#import "BTJTest.h"
+
+@implementation BTJTest
+
+@end
